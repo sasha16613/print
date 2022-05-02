eee834455b758c3536b22722b594210f5ec9c3a9
sasha16613 <nikolay.sasha@mail.ru>
2022-05-02 21:16:03 +0300

    print

---
+++ include/print.h
@@ -0,0 +1,9 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <iostream>
+#include <string>
+
+void print(const std::string& str);
+
+#endif
---
+++ source/print.cpp
@@ -0,0 +1,5 @@
+#include <print.h>
+
+void print(const std::string& str) {
+    std::cout<<str;
+}
